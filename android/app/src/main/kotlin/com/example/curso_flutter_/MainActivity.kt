66e33a109dc5995f02eee76fa5d1785e9e48d84d
Francisco Aparicio Nascimento Coelho <faparicionc@gmail.com>
2025-01-05 09:01:49 -0300

    Ajuste

--- android/app/src/main/kotlin/com/example/curso_flutter_/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.curso_flutter_
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  static const productBaseUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/products';
-  static const orderBaseUrl =
-      'https://shop-cod3r-c70a4-default-rtdb.firebaseio.com/orders';
-  static const webApiKey = 'AIzaSyAnSy0Zm9T3643P7Y2bwSsfyUIzaA_1CZI';
-}
